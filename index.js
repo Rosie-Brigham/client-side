296ae41776c315c532e13f2bf8b7ef005076cce9
Rosie-Brigham <rosiebrigham@gmail.com>
2016-05-01 17:39:43 +1200

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 var xhr = require('xhr')
-var example = require('./views/example.hbs')
+var monarch = require('./views/monarch.hbs')
+var question = require('./views/question.hbs')
+var $ = require('jQuery');
 
-xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
-  if (err) console.log(err) // do something
+document.body.innerHTML = question({});
 
-  console.log(data.body)
-  document.body.innerHTML = example({ name: "Space" });
-})
+$('#question').submit(function( event ) {
+  event.preventDefault();
+  var year = $('#year').val()
 
+  xhr.withCredentials = true;
+  xhr.get('http://localhost:3000/v1/monarchs/' + year, {headers: {"Access-Control-Allow-Origin":  '*' }}, function(err, data) {
+    if (err) console.log(err) // do something
+
+    var monarchData =  JSON.parse(data.body)[0]
+    document.body.innerHTML = monarch({ name: monarchData.name,
+                                        epitaph: monarchData.epitaph,
+                                        house: monarchData.house,
+                                        image_url: monarchData.image_url,
+                                        born: monarchData.born,
+                                        rex_url: monarchData.rex_factor_url,
+                                        died: monarchData.died,
+                                        ruled_from: monarchData.ruled_from,
+                                        ruled_to: monarchData.ruled_to
+                                        });
+  }) //this is always going to be static - so not much use for interaction I guess...
+
+
+});
